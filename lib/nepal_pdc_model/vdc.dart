1c5c2a3cbbc0b2245cfb41f2840957340de4b40e
Aatish Sai <meatish.sai@gmail.com>
2023-02-03 22:31:04 +0545

    Add support for nepali text for vdc

--- lib/nepal_pdc_model/vdc.dart
+++ lib/nepal_pdc_model/vdc.dart
@@ -8,17 +8,21 @@ class Vdc {
   ///name of the local area
   String? text;
 
-  Vdc({this.id, this.districtid, this.text});
+  String? textNp;
+
+  Vdc({this.id, this.districtid, this.text, this.textNp});
 
   factory Vdc.fromJson(Map<String, dynamic> json) => Vdc(
         id: json['id'] as int?,
         districtid: json['districtid'] as int?,
         text: json['text'] as String?,
+        textNp: json['textNP'] as String?
       );
 
   Map<String, dynamic> toJson() => {
         'id': id,
         'districtid': districtid,
         'text': text,
+        'textNP': textNp
       };
 }
